d4570376e1d04afd17d04822b8456fd6118c20fb
wahidAhmad <thesharifi.wahid@gmail.com>
2024-05-28 16:49:19 +0430

    comments
    add JavaScript to comments section

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-// food nav
+/*
+   food nav
+*/
 const indicator = document.querySelector(".nav-indicator");
 const items = document.querySelectorAll(".nav-item");
 
@@ -22,8 +24,9 @@ items.forEach((item, index) => {
    });
    item.classList.contains("is-active") && handleIndicator(item);
 });
-
-// search
+/*
+   search
+*/
 const searchBtn = document.getElementById("pageSearch");
 const searchElement = document.querySelector("#text");
 
@@ -38,15 +41,45 @@ searchBtn.addEventListener("click", function () {
    }
 });
 
-// order
+/*
+   order
+*/
+
 let clickDivs = document.querySelectorAll(".img-about div:last-of-type");
 
 clickDivs.forEach((element) => {
    let clicks = 0;
    element.addEventListener("click", function () {
-      if (clicks < 99) {
+      if (clicks > -1) {
          clicks++;
-         element.textContent = `Ordered ${clicks}`;
+         element.textContent = `added ${clicks}`;
       }
    });
+});
+
+/*
+   comments
+*/
+
+const elements = ['#wahid', '#salaam', '#elyas'].map(id => document.querySelector(id));
+const labels = ['.label1', '.label2', '.label3'].map(cls => document.querySelector(cls));
+
+let counter = 0;
+labels[counter].style.backgroundColor = "#f00";
+
+// Update display and label colors
+const updateComment = () => {
+   elements.forEach((el, index) => el.style.display = index === counter ? "flex" : "none");
+   labels.forEach((label, index) => label.style.backgroundColor = index === counter ? "#f00" : "#000");
+};
+
+// Event listeners
+document.querySelector("#rightArrow").addEventListener("click", () => {
+   counter = (counter + 1) % elements.length;
+   updateComment();
+});
+
+document.querySelector("#leftArrow").addEventListener("click", () => {
+   counter = (counter - 1 + elements.length) % elements.length;
+   updateComment();
 });
